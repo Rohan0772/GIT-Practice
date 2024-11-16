401f39c865a6210c44976554d0849a051f7643fe
rohan <rohan91455@gmail.com>
2024-11-16 16:27:42 +0530

    working on gitplay jenkins integration twice

--- tests/Gitplay.spec.js
+++ tests/Gitplay.spec.js
@@ -11,4 +11,4 @@ for(let i = 0; i <priceMobile.length ; i++){
     console.log( await nameMobile[i].textContent() + "==========>>>>>>" + await priceMobile[i].textContent())
 }
 })
-console.Console.log("changed");
+console.Console.log("changedtwice ");
