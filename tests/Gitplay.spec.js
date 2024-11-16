3052c8c3c9589ffd20e9629ccc47dc2c4991d9e6
rohan <rohan91455@gmail.com>
2024-11-16 16:22:53 +0530

    working on gitplay jenkins integration

--- tests/Gitplay.spec.js
+++ tests/Gitplay.spec.js
@@ -11,3 +11,4 @@ for(let i = 0; i <priceMobile.length ; i++){
     console.log( await nameMobile[i].textContent() + "==========>>>>>>" + await priceMobile[i].textContent())
 }
 })
+console.Console.log("changed");
